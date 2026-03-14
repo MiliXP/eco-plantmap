d1f325ee4f483af5e60f5da495458af4179581ff
Noname191022 <lucasmesquitarage92@outlook.com>
2026-03-13 21:27:46 -0400

    Inicio do projeto FDP

---
+++ src/main/java/com/plantmap/plantmap_backend/PlantmapBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.plantmap.plantmap_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantmapBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantmapBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/plantmap/plantmap_backend/PlantmapBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantmap.plantmap_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantmapBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
