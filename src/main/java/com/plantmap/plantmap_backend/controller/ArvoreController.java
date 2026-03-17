8e49b7c0f6fa1b55dd5b104d7b5307ff2686d0c3
Noname191022 <lucasmesquitarage92@outlook.com>
2026-03-17 17:13:22 -0400

    first commit

---
+++ src/main/java/com/plantmap/plantmap_backend/controller/ArvoreController.java
@@ -0,0 +1,28 @@
+package com.plantmap.controller;
+
+import com.plantmap.model.Arvore;
+import com.plantmap.repository.ArvoreRepository;
+
+import org.springframework.beans.factory.annoyation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestControler
+@RequestMapping("/arvore")
+@CrossOrigin
+public class ArvoreController {
+
+    @Autowired
+    private ArvoreRepository repository;
+
+    @GetMapping
+    public List<Arvore> listar() {
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public Arvore salvar(@RequestBody Arvore arvore) {
+        return repository.save(arvore);
+    }
+}
