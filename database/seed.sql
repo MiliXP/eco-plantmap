6e81d98dd30a4ab5774ee2591f769d89d5483a3d
Drimpe <83505118+Drimpe@users.noreply.github.com>
2026-03-18 14:36:51 -0400

    Delete database directory

--- database/seed.sql
+++
@@ -1,9 +0,0 @@
--- pra ficar organizado, separei os tipos de pontos aqui nesse bloco :P
-
-INSERT INTO tipos_pontos (nome) VALUES
-                                    ('Coleta Seletiva'),
-                                    ('Lixo Eletrônico'),
-                                    ('Recarga de Carro Elétrico'),
-                                    ('Ponto de Plantio');
-
--- OBS: se precisarmos adicionar outros tipos no banco, apenas usaremos o "INSERT INTO tipos_ponto" 
