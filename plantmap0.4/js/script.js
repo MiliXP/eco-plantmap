ef126a8c56a41878afe30e35c29c8d21c5d99452
MiliXP <milianevianasantos678@gmail.com>
2026-03-21 00:15:33 -0400

    Add files via upload

---
+++ plantmap0.4/js/script.js
@@ -0,0 +1,28 @@
+function ordenarRanking() {
+    const tabela = document.querySelector("#tabela-ranking tbody");
+
+    if (!tabela) return; 
+    const linhas = Array.from(tabela.querySelectorAll("tr"));
+
+    linhas.sort((a, b) => {
+        const valorA = parseInt(a.querySelector(".quantidade").textContent.replace(/\D/g, "")) || 0;
+        const valorB = parseInt(b.querySelector(".quantidade").textContent.replace(/\D/g, "")) || 0;
+        return valorB - valorA;
+    });
+
+    tabela.innerHTML = "";
+
+    linhas.forEach((linha, index) => {
+        linha.classList.remove("primeiro");
+
+        linha.children[0].textContent = index + 1;
+
+        if (index === 0) {
+        linha.classList.add("primeiro");
+    }
+
+    tabela.appendChild(linha);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", ordenarRanking);
